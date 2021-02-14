438e1656f7bdaeaf5fa4970d90ec5506a64c44b6
Insan <insan.putranda@gmail.com>
2021-02-14 18:06:59 +0700

    Added lizard and trophy bounce animations. Added attacker scripts and method functions to control speed.

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 1f)] float currentSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * currentSpeed);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+}

f22eba0d2ab02721fdf3fa77904c4590aa4ce980
Insan <insan.putranda@gmail.com>
2021-02-20 15:45:43 +0700

    Snap click position to grid. Update prefab body position.

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -39,6 +39,13 @@ public class DefenderSpawner : MonoBehaviour
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPos;
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+    private Vector2 SnapToGrid(Vector2 worldPos)
+    {
+        float newX = Mathf.RoundToInt(worldPos.x);
+        float newY = Mathf.RoundToInt(worldPos.y);
+        return new Vector2(newX, newY);
     }
 }

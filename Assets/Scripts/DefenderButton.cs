2b30d80cde91cbcc9fd81c746464a93f12385d62
Insan <insan.putranda@gmail.com>
2021-02-20 16:21:14 +0700

    Added defender selection frameworks

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(116, 116, 116, 255);
+        }
+        var spriteRen = GetComponent<SpriteRenderer>();
+        spriteRen.color = Color.white;
+    }
+}

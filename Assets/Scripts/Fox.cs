c6c3777f9a0c1a290256d84d12d060735b7d8760
Insan <insan.putranda@gmail.com>
2021-07-04 18:31:16 +0700

    Added fox behavior jump gravestones.

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+
+        if (otherObject.GetComponent<Defender>())
+        {
+            if (otherObject.GetComponent<Gravestone>())
+            {
+                GetComponent<Animator>().SetTrigger("jumpTrigger");
+                return;
+            }
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}

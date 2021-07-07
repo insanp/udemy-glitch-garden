7a576763fffc3252bb05026bd70f83009eae2478
Insan <insan.putranda@gmail.com>
2021-07-07 17:39:54 +0700

    Fixed win condition, destroy game object on damage collider.

--- Assets/Scripts/DamageCollider.cs
+++ Assets/Scripts/DamageCollider.cs
@@ -6,6 +6,7 @@ public class DamageCollider : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        Destroy(collision.gameObject);
         FindObjectOfType<LivesDisplay>().TakeLives();
     }
 }
